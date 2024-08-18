388ab961e6fde861bea7255853ee9a0d0cf7631f
Bar Amit <bar.amit@outlook.com>
2024-08-18 21:19:45 +0300

    remove old error handling

--- src/allocate_memory.c
+++ src/allocate_memory.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
+
 #include "../include/allocate_memory.h"
-#include "../include/handle_error.h"
 
 void *allocate_memory(long object_size, char *debug_id) {
     void *object_pointer = malloc(object_size);
     if (object_pointer == NULL) {
         return NULL;
-        handle_error(debug_id, "memory allocation fail");
     }
     return object_pointer;
 }
