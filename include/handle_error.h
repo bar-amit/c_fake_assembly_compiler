a69bcc4f5fa7f9d67a8261477f865e3525edfe75
Bar Amit <bar.amit@outlook.com>
2024-08-18 21:17:51 +0300

    add handle error util

--- include/handle_error.h
+++ include/handle_error.h
@@ -1,1 +1,4 @@
-void handle_error(char* function_name, char* error);
+void handle_message(file_head* messages, char* message, int source_line);
+void print_warnings(file_head* warnings, file_head* source_file);
+void print_errors(file_head* errors, file_head* source_file);
+void print_messages(file_head* lines, file_head* source_file, char* message);
--- src/handle_error.c
+++ src/handle_error.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "../include/file_line.h"
 #include "../include/handle_error.h"
 
-void handle_error(char* function_name, char* error){
-        fprintf(stderr, "In %s: ERROR: %s\n", function_name, error);
-        exit(2);
+void handle_message(file_head* messages, char* message, int source_line){
+    if(find_line(messages->head, source_line)==NULL)
+        prepend_line(messages, create_line(message, source_line));
+}
+
+void print_warnings(file_head* warnings, file_head* source_file){
+    print_messages(warnings, source_file, "WARNING");
+}
+void print_errors(file_head* errors, file_head* source_file){
+    print_messages(errors, source_file, "ERROR");
+}
+
+void print_messages(file_head* lines, file_head* source_file, char* message){
+    file_line* current, *source;
+    reverse_file(lines);
+    current = lines->head;
+    while(current!=NULL){
+        source = find_line(source_file->head, current->line_number);
+        printf("%s: %s at line %d:\n", message, current->content, current->line_number);
+        printf("\t%s\n", source->content);
+        current = current->next;
+    }
 }
